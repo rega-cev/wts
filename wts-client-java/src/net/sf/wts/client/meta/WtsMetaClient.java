97200683bd4cd1a6082643a708d10a19931667dc
jvsant1 <none>
2009-08-14 19:08:36 +0200

    svn revision 703 svn date 2007-05-31 21:19:28 added auxilary functions for processing meta xml file

--- wts-client-java/src/net/sf/wts/client/meta/WtsMetaClient.java
+++ wts-client-java/src/net/sf/wts/client/meta/WtsMetaClient.java
@@ -1,8 +1,18 @@
 package net.sf.wts.client.meta;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.sf.wts.client.util.AxisClient;
+
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 
 public class WtsMetaClient 
 {
@@ -59,11 +69,58 @@ public class WtsMetaClient
         return url_;
     }
     
+    private ArrayList<String> parseIONames(byte[] array, String main, String sub)
+    {
+        ArrayList<String> puts = new ArrayList<String>();
+        InputStream is = new ByteArrayInputStream(array);
+        
+        SAXBuilder builder = new SAXBuilder();
+        
+        Document doc = new Document();
+        
+        try
+        {
+            doc = builder.build(is);
+        }
+        catch (JDOMException e1)
+        {
+            e1.printStackTrace();
+        }
+        catch (IOException e1)
+        {
+            e1.printStackTrace();
+        }
+        
+        Element rootElement = doc.getRootElement();
+        
+        List<Element> childrenOfInput = (rootElement.getChild(main)).getChild(sub).getChildren();
+        
+        for(Element e : childrenOfInput)
+        {
+            if(e.getName().equals("name"))
+            {
+                puts.add(e.getValue());
+            }
+        }
+        
+        return puts;
+    }
+    
+    public ArrayList<String> parseInputNames(byte[] array)
+    {
+        return parseIONames(array, "inputs", "input");
+    }
+    
+    public ArrayList<String> parseOutputNames(byte[] array)
+    {
+        return parseIONames(array, "outputs", "output");
+    }
+    
     public static void main(String [] args)
     {
-        WtsMetaClient client = new WtsMetaClient("http://localhost:8080/wts/services/");
+        WtsMetaClient client = new WtsMetaClient("http://zolder:8080/wts/services/");
         System.err.println(client.listServices());
-        byte[] array = client.getServiceDescription("regadb-align");
+        byte[] array = client.getServiceDescription("regadb-hiv-resist");
         System.err.println(new String(array));
     }
 }

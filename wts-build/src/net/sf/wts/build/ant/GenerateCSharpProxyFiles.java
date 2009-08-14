5d92ed1b4c55735ff3a653d998544ce8e116c9ca
jvsant1 <none>
2009-08-14 19:04:48 +0200

    svn revision 510 svn date 2007-04-27 20:51:17 Added ant task to generate c# proxy classes

---
+++ wts-build/src/net/sf/wts/build/ant/GenerateCSharpProxyFiles.java
@@ -0,0 +1,76 @@
+package net.sf.wts.build.ant;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tools.ant.Task;
+
+public class GenerateCSharpProxyFiles  extends Task
+{
+    private File wsdlDirectory;
+    private File csharpDirectory;
+    private File wsdlExecutable;
+    
+    public void execute()
+    {
+        File [] files = wsdlDirectory.listFiles();
+        
+        for(File file : files)
+        {
+            if(file.getAbsolutePath().endsWith(".wsdl"))
+            {
+                String baseName = file.getName();
+                int extensionIndex = baseName.lastIndexOf(".wsdl");
+                baseName = baseName.substring(0, extensionIndex);
+                String wsdlExec = wsdlExecutable.getAbsolutePath();
+                String arg1 = "/out:"+csharpDirectory.getAbsolutePath()+File.separatorChar+baseName+".cs";
+                String arg2 = "/namespace:net.sf.wts.client";
+                String arg3 = file.getAbsolutePath();
+                System.err.println(wsdlExec + " " + arg1+ " " + arg2+ " " + arg3);
+                final ProcessBuilder pb = new ProcessBuilder(wsdlExec, arg1, arg2, arg3);
+
+                try 
+                {
+                    pb.start();
+                } 
+                catch (IOException e) 
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    
+    public static void main(String [] args)
+    {
+        GenerateCSharpProxyFiles antTask = new GenerateCSharpProxyFiles();
+        antTask.setCsharpDirectory(new File("C:\\jvsant1\\wts\\csharp"));
+        antTask.setWsdlDirectory(new File("C:\\jvsant1\\wts\\build"));
+        antTask.setWsdlExecutable(new File("C:\\jvsant1\\wts\\wsdl.exe"));
+        antTask.execute();
+    }
+
+    public File getCsharpDirectory() {
+        return csharpDirectory;
+    }
+
+    public void setCsharpDirectory(File csharpDirectory) {
+        this.csharpDirectory = csharpDirectory;
+    }
+
+    public File getWsdlDirectory() {
+        return wsdlDirectory;
+    }
+
+    public void setWsdlDirectory(File wsdlDirectory) {
+        this.wsdlDirectory = wsdlDirectory;
+    }
+
+    public File getWsdlExecutable() {
+        return wsdlExecutable;
+    }
+
+    public void setWsdlExecutable(File wsdlExecutable) {
+        this.wsdlExecutable = wsdlExecutable;
+    }
+}
